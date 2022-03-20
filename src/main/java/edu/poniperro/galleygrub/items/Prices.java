f94edcde0ff5b96ae927c00165d34d35cac711f9
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-03-20 21:22:54 +0100

    :sparkles: 1st user story done

---
+++ src/main/java/edu/poniperro/galleygrub/items/Prices.java
@@ -0,0 +1,29 @@
+package edu.poniperro.galleygrub.items;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prices {
+    //Atributes
+    private static HashMap<String, Double> extras = new HashMap<String, Double>();
+
+    //Constructor:
+     public Prices(){
+
+     }
+
+    //Methods
+    public static void init_prices(){
+        extras.put("cheese", 0.25);
+        extras.put("sauce", 0.50);
+        extras.put("medium", 0.25);
+        extras.put("large", 0.50);
+    }
+
+
+    public static void display(){
+        for (Map.Entry<String, Double> entry : extras.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue().toString() + " $");
+        }
+    }
+}
